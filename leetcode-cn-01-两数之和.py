45f9f9044f35d27db4240e3b5dcdb7197b17f286
zizaimengzhongyue <luyanfeng1992@qq.com>
2018-05-08 12:48:42 +0800

    第一题

---
+++ leetcode-cn-01-两数之和.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self,nums,target):
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return i,j;
+
+    def test(self):
+        print(self.twoSum([2,7,11,15],9))
+
+temp=Solution();
+temp.test();
