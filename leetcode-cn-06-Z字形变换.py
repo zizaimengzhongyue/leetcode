312e529355eb38c4fdb5230f3ab77a211ec23dfc
zizaimengzhongyue <luyanfeng1992@qq.com>
2018-05-10 13:59:54 +0800

    似乎python的len方法在获取字符串长度的时候复杂度比较高&反正本来超时的代码减少了len的调用就通过了

--- leetcode-cn-06-Z字形变换.py
+++ leetcode-cn-06-Z字形变换.py
@@ -1,28 +1,33 @@
 class Solution:
     def convert(self, s, n):
+        if n <= 1:
+            return s
+        ln = len(s)
         str = ""
-        m = len(s) // (2 * n - 2) * (n - 1)
-        mod = len(s) - len(s) // (2 * n - 2) * (2 * n - 2)
+        m = ln // (2 * n - 2) * (n - 1)
+        mod = ln - ln // (2 * n - 2) * (2 * n - 2)
         if mod == 0:
             m = m
         elif mod < n:
             m += 1
         else:
-            m += mod - n
-        index = 0
-        for i in range(0, n - 1):
-            for j in range(0, m - 1):
-                if j % (n - 1) == 0 or j % (n - 1) == n - i - 1:
+            m += mod - n + 1
+        for i in range(0, n):
+            for j in range(0, m):
+                index = -1
+                if j % (n - 1) == 0:
+                    index = j // (n - 1) * (2 * n - 2) + i
+                elif j % (n - 1) == n - i - 1:
+                    index = j // (n - 1) * (2 * n - 2) + j % (n - 1) + n - 1
+                if index >= 0 and index < ln:
                     str += s[index]
-                    index += 1
-                else:
-                    str += " "
-            str += "\n"
         return str
 
 s = "PAYPALISHIRING"
 numRows = 3
-s = "PAYPALISHIRING"
-numRows = 4
+#s = "PAYPALISHIRING"
+#numRows = 4
+s = "ikicnhlvnsnklobqkfoifwsdzfgoueicotgearzqloawcbomgcwnyosztzoqdyutrbxsuzignicqkresvbgwravnzjdekzabaxvvwddmoheaaiuwlcuddpqebvjlwjmxiiuhnztdgzfhhaftczyhoqmrnxcjirisbezqduuktztdbywezwlnmzfzwjkgatzdxaubvolqpgtrerxdpksbmzckjnrlgqpdqjgnztgqzbjftgkktdmfeqppdgdlysrrdxgermuqogyjmithlhmsychdkkpleicjfinyxkrlqpuexpmlxydvcmapxradzcnfmafjmwwcnqhlityftdcypenownsvrfcdhipuglwuefhmahtwkxuzbhqxuivnnxquhhootnkxstlvaermblnesdcolxbawtvebluuagttbeqbihnlucpmgtcrsdupypvzcrvdxoyysobcxqprshfyahsvmmmkdhmhahkxykxsaavxqkokrvlucfpqtbbwkpfkyqjwcipdimaayrmsmxmredhxgkvqzbcytluvvlydxbfyurqirezvhjtptohtrjtkwngrbgwyjbwdsomjsnfbwaqqqxligeseravujrvsutynimyazdcwfsmwszeadyfwbarmumgofednqcqshuonoclgbrhepmasorgkfnblteamisesmshbwshxjqdaeftvzzfeogrinsuvkapzswaqojpxcuagzvbywhflpwveqpcdprcjoebepqcrkeyjzbkvkrganbggsiljapsqtvuilycxcajbdtxokfqzhwfbfslhhfxabtlmspkuptyuoxiacyzjxhlezylhdkjefwtxlfucyuxorhotipffysjyqwhtisynrtley"
+numRows = 158
 solution = Solution()
 print(solution.convert(s, numRows))
