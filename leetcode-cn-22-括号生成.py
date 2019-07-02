b3cf33d4336dee1905e998892579be112abf8495
zizaimengzhongyue <luyanfeng1992@qq.com>
2019-07-02 17:37:50 +0800

    本地执行报错，但是提交运行没问题，我得看下为什么

---
+++ leetcode-cn-22-括号生成.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def dfs(self, str, left, right):
+        if left + right == self.n and left == right:
+            self.ans.append(str)
+            return
+        if left + right == self.n:
+            return
+        if left > right:
+            self.dfs(str + ')', left, right + 1)
+        self.dfs(str + '(', left + 1, right)
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.n = n * 2
+        self.ans = []
+        left = right = 0
+        self.dfs('', left, right)
+        return self.ans
+
+n = 6
+solution = Solution()
+print(solution.generateParenthesis(n))
