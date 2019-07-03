f3230e846f2a2988cc38da03306c79c298991ccb
zizaimengzhongyue <luyanfeng1992@qq.com>
2019-07-03 21:36:53 +0800

    占个位置

---
+++ leetcode-cn-23-合并K个排序链表.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        ans = None
+
+
