e78e965fe8f65088fed8f438fc618509cf2316c2
zizaimengzhongyue <luyanfeng1992@qq.com>
2019-07-07 17:55:00 +0800

    题目意思容易搞错

--- leetcode-cn-26-删除排序数组中的重复项.py
+++ leetcode-cn-26-删除排序数组中的重复项.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from typing import List
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        ln = 0
+        for i in range(0, len(nums)):
+            if i - 1 >= 0 and nums[i] == nums[i-1]:
+                continue
+            nums[ln] = nums[i]
+            ln = ln + 1
+        return ln
+
+nums = [1,1,2]
+solution = Solution()
+print(solution.removeDuplicates(nums))
