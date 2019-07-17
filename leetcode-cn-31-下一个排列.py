3ea0d172e0d058e78fa539cb041da1b2d9da3480
zizaimengzhongyue <luyanfeng1992@qq.com>
2019-07-17 20:19:42 +0800

    脑子不清醒

--- leetcode-cn-31-下一个排列.py
+++ leetcode-cn-31-下一个排列.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from typing import *
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         ln = len(nums)
-        temp = nums[ln - 1]
+        flag = False
         for i in range(ln - 1, -1, -1):
-            if nums[i] < temp:
-                nums[ln - 1] = nums[i]
-                nums[i] = temp
+            for j in range(i, ln):
+                if nums[j] > nums[i]:
+                    temp = nums[j]
+                    nums[j] = nums[i]
+                    nums[i] = temp
+                    flag = True
+                    break
+            if flag:
                 break
+            for j in range(i, ln - 1):
+                if nums[j] > nums[j + 1]:
+                    temp = nums[j]
+                    nums[j] = nums[j + 1]
+                    nums[j + 1] = temp
+                else:
+                    break
 
+nums = [1,2,3]
+nums = [2,3,1]
+nums = [3,2,1]
+nums = [4,2,0,2,3,2,0]
+#nums = [1,3,2]
+#nums = [3,2,1]
+solution = Solution()
+solution.nextPermutation(nums)
+print(nums)
