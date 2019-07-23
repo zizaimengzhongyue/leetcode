5daf57cb37eadaf2067985ada62c8f9e71b151a4
zizaimengzhongyue <luyanfeng1992@qq.com>
2019-07-24 00:20:25 +0800

    灌水

--- leetcode-cn-46-全排列.py
+++ leetcode-cn-46-全排列.py
@@ -4,6 +4,29 @@
 class Solution:
 
     def dfs(self, nums, ans, temp):
+        if len(temp) == len(nums):
+            ans.append(temp.copy())
+            return
+        for i in range(0, len(nums)):
+            if self.mark[i] == 0:
+                temp.append(nums[i])
+                self.mark[i] = 1
+                self.dfs(nums, ans, temp)
+                self.mark[i] = 0
+                temp.pop()
+        return
 
     
     def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        self.mark = []
+        for i in range(0, len(nums)):
+            self.mark.append(0)
+        for i in range(0, len(nums)):
+            temp = []
+            temp.append(nums[i])
+            self.mark[i] = 1
+            self.dfs(nums, ans, temp)
+            self.mark[i] = 0
+            temp.pop()
+        return ans
