3ad9d094a1727525ab1a402dcaa300ca046306fc
zizaimengzhongyue <luyanfeng1992@qq.com>
2020-04-13 18:30:22 +0800

    排序下就行

---
+++ leetcode-cn-75-颜色分类.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sortColors(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums)-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+}
+
+func main() {
+	nums := []int{2, 0, 2, 1, 1, 0}
+	sortColors(nums)
+	fmt.Println(nums)
+}
