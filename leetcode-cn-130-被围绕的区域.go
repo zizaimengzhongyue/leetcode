0f6a442c4eaedfdfa05bc181b125e884ad3738bc
zizaimengzhongyue <luyanfeng1992@qq.com>
2021-04-28 11:37:54 +0800

    编码更简介的解法

--- leetcode-cn-130-被围绕的区域.go
+++ leetcode-cn-130-被围绕的区域.go
@@ -69,22 +69,57 @@ func solve(board [][]byte) {
 	}
 }
 
+func solve02(b [][]byte) {
+	if len(b) == 0 || len(b[0]) == 0 {
+		return
+	}
+	m := len(b)
+	n := len(b[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if b[i][j] == 'O' && (i == 0 || i == m-1 || j == 0 || j == n-1) {
+				dfs(b, i, j)
+			}
+		}
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if b[i][j] == '-' {
+				b[i][j] = 'O'
+			} else {
+				b[i][j] = 'X'
+			}
+		}
+	}
+}
+
+func dfs(b [][]byte, i int, j int) {
+	if i < 0 || i >= len(b) || j < 0 || j >= len(b[0]) || b[i][j] != 'O' {
+		return
+	}
+	b[i][j] = '-'
+	dfs(b, i+1, j)
+	dfs(b, i-1, j)
+	dfs(b, i, j+1)
+	dfs(b, i, j-1)
+}
+
 func main() {
 	b := [][]byte{}
 	b = append(b, []byte{'X', 'X', 'X', 'X'})
 	b = append(b, []byte{'X', 'O', 'O', 'X'})
 	b = append(b, []byte{'X', 'X', 'O', 'X'})
 	b = append(b, []byte{'X', 'O', 'X', 'X'})
-	solve(b)
+	solve02(b)
 	fmt.Println(b)
 
 	b = [][]byte{[]byte{'X'}}
-	solve(b)
+	solve02(b)
 	fmt.Println(b)
 
 	b = [][]byte{}
 	b = append(b, []byte{'O', 'O'})
 	b = append(b, []byte{'O', 'O'})
-	solve(b)
+	solve02(b)
 	fmt.Println(b)
 }
